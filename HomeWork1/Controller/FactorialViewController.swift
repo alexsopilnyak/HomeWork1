cdbc5f76d040d25ac19859a5b5f211145d76f442
Alexandr Sopilnyk <sopilnyak.alexandr@gmail.com>
2020-12-06 21:39:04 +0200

    add ui and controller functionality to factorial module

--- HomeWork1/Controller/FactorialViewController.swift
+++ HomeWork1/Controller/FactorialViewController.swift
@@ -9,8 +9,32 @@ import UIKit
 
 class FactorialViewController: UIViewController {
   
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var resultLabel: UILabel!
+  @IBOutlet weak var recursionSwitch: UISwitch!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  @IBAction func calculateButtonPressed(_ sender: UIButton) {
+    if let numberFromTextField = textField.text {
+      guard let number = Int(numberFromTextField) else { print("Casting error")
+        self.resultLabel.text = "Could not cast number to Int"
+        return
+      }
+      
+      if recursionSwitch.isOn {
+        resultLabel.text = FactorialCalculator.recursionCalculation(of: number).description
+      } else {
+        resultLabel.text = FactorialCalculator.iterationCalculation(of: number).description
+      }
+    }
+  }
+  
+  @IBAction func clearButtonPressed(_ sender: UIButton) {
+    textField.text = ""
+    resultLabel.text = ""
   }
   
   
