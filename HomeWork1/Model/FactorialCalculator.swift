38d55eb939bd34ed84021003acc52d1886641375
Alexandr Sopilnyk <sopilnyak.alexandr@gmail.com>
2020-12-06 19:19:17 +0200

    Add iteration and recursion calculation of factorial

--- HomeWork1/Model/FactorialCalculator.swift
+++ HomeWork1/Model/FactorialCalculator.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct FactorialCalculator {
+  
+  static func iterationCalculation(of number: Int) -> Int {
+    var result = 1
+    
+    if number == 0 {
+      return 0
+    } else {
+      for i in 1...number {
+        result *= i
+      }
+      
+      return result
+    }
+  }
+  
+  static func recursionCalculation(of number: Int) -> Int {
+    
+    switch number {
+    case 0: return 0
+    case 1: return 1
+    default:
+      return number * recursionCalculation(of: number - 1)
+    }
+  }
+  
+}
