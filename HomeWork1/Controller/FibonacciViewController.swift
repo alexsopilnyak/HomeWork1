dbfd8d931245b407db1c09f8cf2fdcac63e9586d
Alexandr Sopilnyk <sopilnyak.alexandr@gmail.com>
2020-12-06 21:01:02 +0200

    add controller logic and ui for task 2

--- HomeWork1/Controller/FibonacciViewController.swift
+++ HomeWork1/Controller/FibonacciViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class FibonacciViewController: UIViewController {
   
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var resultLabel: UILabel!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
+  @IBAction func calculateButtonPressed(_ sender: UIButton) {
+    
+    if let numberFromTextField = textField.text {
+      guard let number = Int(numberFromTextField) else{print("Casting error"); return}
+      
+      let fibonacciSequence = Fibonacci().fibonacciSequence(till: number)
+      resultLabel.text = fibonacciSequence.description
+    }
+    
+    
+    
+    
+    
+    
+    
+  }
   
 }
