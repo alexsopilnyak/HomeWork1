4d2d58bdec257c5efdc6595af32341c37da9caf3
Alexandr Sopilnyk <sopilnyak.alexandr@gmail.com>
2020-12-06 19:09:06 +0200

    Add MVC

---
+++ HomeWork1/Model/Fibonacci.swift
@@ -0,0 +1,8 @@
+//
+//  Fibonacci.swift
+//  HomeWork1
+//
+//  Created by Alexandr Sopilnyak on 06.12.2020.
+//
+
+import Foundation
---
+++ HomeWork1/Model/PiPresentation.swift
@@ -0,0 +1,8 @@
+//
+//  PiPresentation.swift
+//  HomeWork1
+//
+//  Created by Alexandr Sopilnyak on 06.12.2020.
+//
+
+import Foundation
