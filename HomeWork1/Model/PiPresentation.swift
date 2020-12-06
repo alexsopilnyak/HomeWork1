e110157048666a04ef1773350ddb6909fd16347a
Alexandr Sopilnyk <sopilnyak.alexandr@gmail.com>
2020-12-06 20:01:54 +0200

    add finding N digit of PI

--- HomeWork1/Model/PiPresentation.swift
+++ HomeWork1/Model/PiPresentation.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct PiPresentation {
+  
+  
+  
+  func digitOfPi(numberOfDigits: Int, indexOfDigit: Int) -> Character? {
+    let piPresentedByString = String(format: "%.\(numberOfDigits)f", Double.pi)
+    
+    if indexOfDigit > piPresentedByString.count - 1 || numberOfDigits < 0 {
+      return nil
+    } else {
+      let digitsArray = piPresentedByString.map {$0}
+      
+      return digitsArray[indexOfDigit]
+      
+    }
+  }
+  
+}
